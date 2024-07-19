04db6be781e3a69c1380f0a1aa0cea9eec9cbd1e
dogasantos <douglas.santos@csurface.net>
2024-07-19 17:42:58 -0300

    teste

--- pkg/plugins/services/mysql/mysql.go
+++ pkg/plugins/services/mysql/mysql.go
@@ -1,17 +1,3 @@
-// Copyright 2022 Praetorian Security, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package mysql
 
 import (
@@ -23,44 +9,9 @@ import (
 	utils "github.com/praetorian-inc/fingerprintx/pkg/plugins/pluginutils"
 )
 
-/*
-When we perform fingerprinting of the MySQL service, we can expect to get one
-of two packets back from the server on the initial connection. The first would
-be an initial handshake packet indicating we can authenticate to the server.
-
-The second potential response would be an error message returned by the server
-telling us why we can't authenticate. For example, the server may respond with
-an error message stating the client IP is not allowed to authenticate to the
-server.
-
- Example MySQL Initial Handshake Packet:
-   Length: 4a 00 00 00
-   Version: 0a
-   Server Version: 38 2e 30  2e 32 38 00 (null terminated string "8.0.28")
-   Connection Id: 0b 00 00 00
-   Auth-Plugin-Data-Part-1: 15 05 6c 51 28 32 48 15
-   Filler: 00
-   Capability Flags: ff ff
-   Character Set: ff
-   Status Flags: 02 00
-   Capability Flags: ff df
-   Length of Auth Plugin Data: 15
-   Reserved (all 00): 00 00 00 00 00 00 00 00 00 00
-   Auth-Plugin-Data-Part-2 (len 13 base 10): 26 68 15 1e 2e 7f 69 38 52 6b 6c 5c 00
-   Auth Plugin Name: null terminated string "caching_sha2_password"
-
- Example MySQL Error Packet on Initial Connection:
-   Packet Length: 45 00 00 00
-   Header: ff
-   Error Code: 6a 04
-   Human Readable Error Message: Host '50.82.91.234' is not allowed to connect to this MySQL server
-*/
-
 type MYSQLPlugin struct{}
 
 const (
-	// protocolVersion = 10
-	// maxPacketLength = 1<<24 - 1
 	MYSQL = "MySQL"
 )
 
@@ -68,10 +19,6 @@ func init() {
 	plugins.RegisterPlugin(&MYSQLPlugin{})
 }
 
-// Run checks if the identified service is a MySQL (or MariaDB) server using
-// two methods. Upon the connection of a client to a MySQL server it can return
-// one of two responses. Either the server returns an initial handshake packet
-// or an error message packet.
 func (p *MYSQLPlugin) Run(conn net.Conn, timeout time.Duration, target plugins.Target) (*plugins.Service, error) {
 	response, err := utils.Recv(conn, timeout)
 	if err != nil {
@@ -119,10 +66,7 @@ func (p *MYSQLPlugin) Priority() int {
 	return 133
 }
 
-// CheckErrorMessagePacket checks the response packet error message
 func CheckErrorMessagePacket(response []byte) (string, int, error) {
-	// My brief research suggests that its not possible to get a compliant
-	// error message packet that is less than eight bytes
 	if len(response) < 8 {
 		return "", 0, &utils.InvalidResponseErrorInfo{
 			Service: MYSQL,
@@ -130,17 +74,7 @@ func CheckErrorMessagePacket(response []byte) (string, int, error) {
 		}
 	}
 
-	packetLength := int(
-		uint32(
-			response[0],
-		) | uint32(
-			response[1],
-		)<<8 | uint32(
-			response[2],
-		)<<16 | uint32(
-			response[3],
-		)<<24,
-	)
+	packetLength := int(uint32(response[0]) | uint32(response[1])<<8 | uint32(response[2])<<16 | uint32(response[3])<<24)
 	actualResponseLength := len(response) - 4
 
 	if packetLength != actualResponseLength {
@@ -174,11 +108,7 @@ func CheckErrorMessagePacket(response []byte) (string, int, error) {
 	return errorStr, errorCode, nil
 }
 
-// CheckInitialHandshakePacket checks if the response received from the server
-// matches the expected response for the MySQL service
 func CheckInitialHandshakePacket(response []byte) (string, error) {
-	// My brief research suggests that its not possible to get a compliant
-	// initial handshake packet that is less than roughly 35 bytes
 	if len(response) < 35 {
 		return "", &utils.InvalidResponseErrorInfo{
 			Service: MYSQL,
@@ -186,17 +116,7 @@ func CheckInitialHandshakePacket(response []byte) (string, error) {
 		}
 	}
 
-	packetLength := int(
-		uint32(
-			response[0],
-		) | uint32(
-			response[1],
-		)<<8 | uint32(
-			response[2],
-		)<<16 | uint32(
-			response[3],
-		)<<24,
-	)
+	packetLength := int(uint32(response[0]) | uint32(response[1])<<8 | uint32(response[2])<<16 | uint32(response[3])<<24)
 	version := int(response[4])
 
 	if packetLength < 25 || packetLength > 4096 {
@@ -221,22 +141,19 @@ func CheckInitialHandshakePacket(response []byte) (string, error) {
 		}
 	}
 
-	// If we skip the connection id and auth-plugin-data-part-1 fields the spec says
-	// there is a filler byte that should always be zero at this position
 	fillerPos := position + 13
-	if position >= len(response) {
+	if fillerPos >= len(response) {
 		return "", &utils.InvalidResponseErrorInfo{
 			Service: MYSQL,
 			Info:    "buffer is too small to be a valid initial handshake packet",
 		}
 	}
 
-	// According to the specification this should always be zero since it is a filler byte
 	if response[fillerPos] != 0x00 {
 		return "", &utils.InvalidResponseErrorInfo{
 			Service: MYSQL,
 			Info: fmt.Sprintf(
-				"expected filler byte at ths position to be zero got: %d",
+				"expected filler byte at this position to be zero got: %d",
 				response[fillerPos],
 			),
 		}
@@ -245,8 +162,6 @@ func CheckInitialHandshakePacket(response []byte) (string, error) {
 	return mysqlVersionStr, nil
 }
 
-// readNullTerminatedASCIIString is responsible for reading a null terminated
-// ASCII string from a buffer and returns it as a string type
 func readNullTerminatedASCIIString(buffer []byte, startPosition int) (string, int, error) {
 	characters := []byte{}
 	success := false
@@ -274,8 +189,6 @@ func readNullTerminatedASCIIString(buffer []byte, startPosition int) (string, in
 	return string(characters), endPosition, nil
 }
 
-// readEOFTerminatedASCIIString is responsible for reading an ASCII string
-// that is terminated by the end of the message
 func readEOFTerminatedASCIIString(buffer []byte, startPosition int) (string, error) {
 	characters := []byte{}
 
