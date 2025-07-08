5153aff5c68aa6cd79ab3b33eb57f5ffda8f1145
dogasantos <douglas.santos@csurface.net>
2025-07-07 21:05:19 -0300

    tae

--- pkg/plugins/services/l2tp/l2tp.go
+++ pkg/plugins/services/l2tp/l2tp.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Praetorian Security, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package l2tp
 
 import (
@@ -281,8 +295,6 @@ func parseL2TPInfo(response []byte) (map[string]any, string) {
 	length := binary.BigEndian.Uint16(response[2:4])
 	tunnelID := binary.BigEndian.Uint16(response[4:6])
 	sessionID := binary.BigEndian.Uint16(response[6:8])
-	ns := binary.BigEndian.Uint16(response[8:10])
-	nr := binary.BigEndian.Uint16(response[10:12])
 
 	version := flags & 0x000F
 	info["L2TP_Version"] = fmt.Sprintf("%d", version)
@@ -299,7 +311,7 @@ func parseL2TPInfo(response []byte) (map[string]any, string) {
 	for offset < len(response)-6 {
 		// Parse AVP header
 		avpFlags := binary.BigEndian.Uint16(response[offset : offset+2])
-		vendorID := binary.BigEndian.Uint16(response[offset+2 : offset+4])
+		binary.BigEndian.Uint16(response[offset+2 : offset+4]) // vendorID - read but not used
 		avpLength := binary.BigEndian.Uint16(response[offset+4 : offset+6])
 
 		if avpLength < 6 || offset+int(avpLength) > len(response) {
